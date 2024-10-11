5b3cff125828d1d42d20b10306fa75984b947dfa
Yousef Reda <sefy23624@gmail.com>
2024-10-11 20:15:34 +0300

    main project

--- program.py
+++ program.py
@@ -1,1 +1,12 @@
-print("hello world!")
+
+"""add=lambda x:x**x
+print(add(10))
+max=lambda x,y: x if x>y else y
+print(max(10, 20)) 
+
+examples=[{'name':'yousef','age':122,'hight':200},{'name':'yousef2','age':122,'hight':200}]
+print(examples[1])
+"""
+num =[10,50,60,70,80,90,100]
+x=list(map(lambda x:x**2,num))
+print(x)
